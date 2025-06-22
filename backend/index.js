943cba13519abc6eaf3b03b0bec60a6bad0d2db0
Rafi3 <rafi3ramadhan@gmail.com>
2025-06-23 06:03:53 +0700

    change origin link to new fork deployment

--- backend/index.js
+++ backend/index.js
@@ -4,25 +4,27 @@ const cors = require("cors");
 
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
-app.use(cors({
-    origin: ['https://bookstore-mern-ppl.vercel.app'],
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: ["https://klodran-bookstore-ppl.vercel.app"],
+    credentials: true,
+  })
+);
 
 // routes
-const bookRoutes = require('./src/books/book.route');
-const orderRoutes = require("./src/orders/order.route")
-const userRoutes =  require("./src/users/user.route")
-const adminRoutes = require("./src/stats/admin.stats")
+const bookRoutes = require("./src/books/book.route");
+const orderRoutes = require("./src/orders/order.route");
+const userRoutes = require("./src/users/user.route");
+const adminRoutes = require("./src/stats/admin.stats");
 
-app.use("/api/books", bookRoutes)
-app.use("/api/orders", orderRoutes)
-app.use("/api/auth", userRoutes)
-app.use("/api/admin", adminRoutes)
+app.use("/api/books", bookRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/admin", adminRoutes);
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
@@ -31,7 +33,9 @@ async function main() {
   });
 }
 
-main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+main()
+  .then(() => console.log("Mongodb connect successfully!"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
